e19a9c43234b65053becc694eac3e1b70448284c
Whylee <xiaowen20040813@163.com>
2020-10-08 21:40:56 +0800

    添加log

--- app/src/main/java/cn/popmama/tcmd1/MainActivity.java
+++ app/src/main/java/cn/popmama/tcmd1/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Log.i("tag","aaa ");
-        Log.i("tag","dev branch");
+        Log.i("tag","dev modify 2222 branch");
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
