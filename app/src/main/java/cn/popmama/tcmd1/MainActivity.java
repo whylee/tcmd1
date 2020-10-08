53580596ba30d8b98d6d3451bb4f31c3e689f1c3
Whylee <xiaowen20040813@163.com>
2020-10-08 21:22:08 +0800

    fix change 111

--- app/src/main/java/cn/popmama/tcmd1/MainActivity.java
+++ app/src/main/java/cn/popmama/tcmd1/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Log.i("tag","aaa ");
-        Log.i("tag","ff&&main branch");
+        Log.i("tag","ff&&main change 111 branch");
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
